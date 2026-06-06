2ce228a997ef36f07f0a9cbfddc84c5b63d24b10
monju1125 <monju1125@github.com>
2026-06-06 20:32:26 +0000

    Test run: 2/2 passed, 0 failed, 0 skipped - 2026-06-06T20:32:26.310Z

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post(id INT PRIMARY KEY NOT NULL, post TEXT(100), user_fk INT REFERENCES site_user(id) ON DELETE CASCADE );
+CREATE TABLE post(id serial PRIMARY KEY, post TEXT(100), user_fk INT REFERENCES site_user(id) ON DELETE CASCADE );
