5fbcd9b6198b59f82625489e83620769bc5a26ff
monju1125 <monju1125@github.com>
2026-06-06 20:30:19 +0000

    Test run: 1/2 passed, 1 failed, 0 skipped - 2026-06-06T20:30:19.076Z

--- problem1.sql
+++ problem1.sql
@@ -1,1 +1,1 @@
-CREATE TABLE post(id INT PRIMARY KEY NOT NULL, post VARCHAR(100), user_fk INT, FOREIGN KEY(user_fk) REFERENCES site_user(id) );
+CREATE TABLE post(id INT PRIMARY KEY NOT NULL, post VARCHAR(100), user_fk INT REFERENCES site_user(id) );
